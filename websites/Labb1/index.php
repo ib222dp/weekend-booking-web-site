1cceac878b86cd1125a9f8b5b39fffeab88700fa
ib222dp <ib222dp@student.lnu.se>
2015-11-04 08:47:28 +0100

    First commit

---
+++ websites/Labb1/index.php
@@ -0,0 +1,13 @@
+<?php
+
+//curl_get_request("localhost:8080");
+
+/*function curl_get_request($url){
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+
+    $data = curl_exec($ch);
+    curl_close($ch); 
+}*/
+
+phpinfo();
